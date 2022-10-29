17ed57436f1669962129f7ad512754836a841a84
kentodaikoku <kedks090597@gmail.com>
2022-10-29 16:05:15 +0900

    humburger menu

---
+++ index.js
@@ -0,0 +1,20 @@
+// ハンバーガーメニュー
+$(function() {
+  $('#nav-btn').on('click', function() {
+    alert('aaaa');
+  })
+});
+
+// $(function() {
+//   $('.nav_btn').on('click', function() {
+//     if ($('header').hasClass('open')) {
+//       $('header').removeClass('open')
+//     } else {
+//       $('#header').addClass('open');
+//     }
+//   });
+
+//   $('#mask').on('click', () => {
+//     $('header').removeClass('open');
+//   });
+// });
