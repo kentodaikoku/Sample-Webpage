85b9d6245bfb0c3e0ea707d92c4f271cb60d6b45
kentodaikoku <kedks090597@gmail.com>
2022-10-30 23:53:47 +0900

    main.js, navmenu

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 // ハンバーガーメニュー
 $(function() {
   $('#nav-btn').on('click', function() {
-    alert('aaaa');
-  })
+    // alert('aaaa');
+    if ($('header').hasClass('open')) {
+      $('header').removeClass('open');
+    } else {
+      $('header').addClass('open');
+    }
+  });
+
+  $('#mask').on('click', function() {
+    $('header').removeClass('open');
+  });
 });
-
-// $(function() {
-//   $('.nav_btn').on('click', function() {
-//     if ($('header').hasClass('open')) {
-//       $('header').removeClass('open')
-//     } else {
-//       $('#header').addClass('open');
-//     }
-//   });
-
-//   $('#mask').on('click', () => {
-//     $('header').removeClass('open');
-//   });
-// });
