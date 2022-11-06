9107200bdac91ab1523c2c05f96ed6aad9a0a545
kentodaikoku <kedks090597@gmail.com>
2022-11-06 15:58:21 +0900

    調整

--- index.js
+++ index.js
@@ -1,15 +1,51 @@
 // ハンバーガーメニュー
+
 $(function() {
   $('#nav-btn').on('click', function() {
     // alert('aaaa');
-    if ($('header').hasClass('open')) {
-      $('header').removeClass('open');
+    if ($('#header').hasClass('open')) {
+      $('#header').removeClass('open');
     } else {
-      $('header').addClass('open');
+      $('#header').addClass('open');
     }
   });
 
   $('#mask').on('click', function() {
-    $('header').removeClass('open');
+    $('#header').removeClass('open');
   });
 });
+
+
+// slick
+
+$('.slick-center').slick({
+  arrows: false,
+  centerMode: true,
+  centerPadding: '85px',
+  slidesToShow: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '50px',
+        slidesToShow: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+// TODO: ページ移動アニメーション
+
+// TODO：　スクロール時画像フェード表示
+
